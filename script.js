1a64f6392f91b89e7baf45a49cf67bd55901465e
LarsMartin-Lillerovde <239625292+dalsfjord-blip@users.noreply.github.com>
2026-02-23 14:48:26 +0100

    Last commit before extra credits. Assignment done in terms of button regenarating grid size, and hover function working.

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const container = document.querySelector(".grid-container");
-const GRID_SIZE = 16;
-const totalCells = GRID_SIZE * GRID_SIZE;
+const newGridBtn = document.getElementById('new-grid-btn');
 
-function createGrid() {
-    for (let i = 0; i < totalCells; i++) {
+// Create an initial 16x16 grid on page load
+createGrid(16);
+
+function createGrid(squaresPerSide) {
+   
+    container.innerHTML = "";
+
+   r
+    const sizePercentage = 100 / squaresPerSide;
+
+   
+    const totalSquares = squaresPerSide * squaresPerSide;
+    
+    for (let i = 0; i < totalSquares; i++) {
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
+        
+        
+        gridItem.style.width = `${sizePercentage}%`;
+        gridItem.style.height = `${sizePercentage}%`;
+
         container.appendChild(gridItem);
     }
 }
 
-createGrid();
+// 4. Button Logic
+newGridBtn.addEventListener("click", () => {
+    let userInput = prompt("How many squares per side do you want? (Max 100)");
+    let size = parseInt(userInput);
+
+    
+    if (isNaN(size) || size < 1 || size > 100) {
+        alert("Error: Please enter a number between 1 and 100.");
+    } else {
+        createGrid(size);
+    }
+});
