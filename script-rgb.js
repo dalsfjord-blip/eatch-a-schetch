eb1318bf797d04ebde95f175d05dd10aa449542d
LarsMartin-Lillerovde <239625292+dalsfjord-blip@users.noreply.github.com>
2026-02-24 09:05:05 +0100

    Extra credit assignment is in the "-rgb" files. Just change them in the "index.html" to see result.

--- script-rgb.js
+++ script-rgb.js
@@ -5,13 +5,10 @@ const newGridBtn = document.getElementById('new-grid-btn');
 createGrid(16);
 
 function createGrid(squaresPerSide) {
-   
-    container.innerHTML = "";
+   container.innerHTML = "";
 
    
     const sizePercentage = 100 / squaresPerSide;
-
-   
     const totalSquares = squaresPerSide * squaresPerSide;
     
     for (let i = 0; i < totalSquares; i++) {
@@ -29,7 +26,12 @@ function createGrid(squaresPerSide) {
         container.appendChild(gridItem);
     }
 }
-
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
 // 4. Button Logic
 newGridBtn.addEventListener("click", () => {
     let userInput = prompt("How many squares per side do you want? (Max 100)");
